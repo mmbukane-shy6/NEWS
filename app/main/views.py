b1c4b8b372face8b41d35be556b6e0bc40e4e7aa
sheila egeidza <sheiladidy9@gmail.com>
2020-05-01 18:24:10 +0300

    added index.css file

---
+++ app/main/views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from ..request import get_news
+from. import main
+
+
+@main.route('/')
+def index():
+    '''
+    view page functions that returns an index page and its data
+    '''
+
+    latest_news = get_news('latest')
+    title = 'News Highlight application'
+    return render_template('index.html', title=title, latest=latest_news)
+
+
+@main.route('/new/<int:id>')
+def new(id):
+    '''
+     view page that returns news details and its data
+    '''
+
+    new = get_new(id)
+    title = f'{new.title}'
+
+    return render_template('news.html', title=title, new=new)
