7bbec088d33616fc4f0e6c505c707691ebdcf952
sheila egeidza <sheiladidy9@gmail.com>
2020-05-01 23:53:36 +0300

    modified error.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-from app import App
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
@@ -12,12 +11,13 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    bootstrap.init_app(app)
+
     #initialise blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # Initializing flask extensions
-    bootstrap.init_app(app)
     from .request import configure_request
     configure_request(app)
     # Will add the views and forms
--- app/test/news_test.py
+++ app/test/news_test.py
@@ -1,6 +1,6 @@
 import unittest
-from models import News
-News = news.News
+from app.models import News
+
 
 class NewsTest(unittest.TestCase):
     '''
