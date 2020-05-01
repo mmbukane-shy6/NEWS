068a0cb6d2bd7c1544f69e0e829c6a9187a51605
sheila egeidza <sheiladidy9@gmail.com>
2020-05-01 18:43:47 +0300

    imported config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from config import config_options
+
+  
 
 # Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__ )
 
-from app import views
+    #setting up configuration
+    app.config.from_object(config_options[config_name])
+   
+    
+    #registering a blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    #setting config
+    from .requests import configure_request
+    configure_request(app)
+    
+    
+    #will return the app
+    return app
